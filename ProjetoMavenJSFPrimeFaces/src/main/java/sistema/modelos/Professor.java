e485540d1c8b954791334697e72ad35d5f56a161
Eduardo <Eduardo@192.168.0.16>
2018-02-28 15:26:47 -0300

    Colocada a classe Professor no package modelos

---
+++ ProjetoMavenJSFPrimeFaces/src/main/java/sistema/modelos/Professor.java
@@ -0,0 +1,27 @@
+package sistema.modelos;
+
+public class Professor 
+{
+	private int codigo;
+	private String nome;
+	private String disciplina;
+	
+	public int getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getDisciplina() {
+		return disciplina;
+	}
+	public void setDisciplina(String disciplina) {
+		this.disciplina = disciplina;
+	}
+}
